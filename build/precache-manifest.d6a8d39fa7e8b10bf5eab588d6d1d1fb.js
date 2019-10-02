fb20a7111537f06b9f6c0e399ba954477cce2dc7
208161088 <34226771+208161088@users.noreply.github.com>
2019-10-02 18:56:01 +0300

    change

---
+++ build/precache-manifest.d6a8d39fa7e8b10bf5eab588d6d1d1fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e5f544461d5ecb70f879017e389b01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b62c65f6624c7d49cb7",
+    "url": "/static/css/main.9e25093e.chunk.css"
+  },
+  {
+    "revision": "a2daa37513650682ea83",
+    "url": "/static/js/2.0238f660.chunk.js"
+  },
+  {
+    "revision": "4b62c65f6624c7d49cb7",
+    "url": "/static/js/main.2834e894.chunk.js"
+  },
+  {
+    "revision": "1834fba7d454f19c1452",
+    "url": "/static/js/runtime-main.70956e8b.js"
+  },
+  {
+    "revision": "40ff87b129e86e1fa4e876f693f1bd4d",
+    "url": "/static/media/image_not_found.40ff87b1.webp"
+  },
+  {
+    "revision": "a5e94c7fddd49dfe7a4f245de11772df",
+    "url": "/static/media/no_image_available.a5e94c7f.webp"
+  },
+  {
+    "revision": "ee797453a8e41281a01dd7b61a6e2bcd",
+    "url": "/static/media/star_wars_header.ee797453.webp"
+  }
+]);
